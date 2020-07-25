fad5e35042306a88e2388115204ddcc6c9ade4a7
t-gebauer <t-gebauer@users.noreply.github.com>
2020-07-25 16:15:35 +0200

    Parse a simple data class

--- index.js
+++ index.js
@@ -6,40 +6,82 @@ const { log } = require('console')
 const parser = new Parser()
 parser.setLanguage(Kotlin)
 
-
 const sourceCode = fs.readFileSync("test/Apple.kt").toString()
 const tree = parser.parse(sourceCode)
 
-log("Complete tree:")
-log(tree.rootNode.toString())
-log()
-
-const cursor = tree.walk()
-
-cursor.gotoFirstChild()
-
-function walkUntil(type) {
-    let didWalk = true
-    while (didWalk && cursor.currentNode.type !== type) {
-        didWalk = cursor.gotoNextSibling()
+class Parameter {
+    /**
+     * @param {string} mutableType
+     * @param {string} identifier
+     * @param {string} type
+     * @param {string} isNullable
+     */
+    constructor(mutableType, identifier, type, isNullable) {
+        this.mutableType = mutableType;
+        this.identifier = identifier;
+        this.type = type;
+        this.isNullable = isNullable;
     }
 }
 
-// Find class declaration
-walkUntil('class_declaration')
-cursor.gotoFirstChild()
+/**
+ *  @param {string} type
+ */
+function mapType(type) {
+    switch (type) {
+        case 'Byte':
+        case 'Short':
+        case 'Int':
+        case 'Long':
+        case 'Float':
+        case 'Double':
+            return 'number'
+        case 'String':
+            return 'string'
+    }
+}
 
-// Find class name
-walkUntil('type_identifier')
-const className = cursor.currentNode.text
-log("Class name:", className)
+/**
+ * @param {Parser.SyntaxNode} node
+ */
+function createParameterFromNode(node) {
+    const firstChildNode = node.children[0]
+    if (firstChildNode.type === 'val' || firstChildNode.type === 'var') {
+        const mutableType = firstChildNode.text
+        const identifierNode = node.children[1]
+        const identifier = identifierNode.text
+        let typeNode = node.children[3]
+        const isNullable = typeNode.type === 'nullable_type'
+        if (isNullable) {
+            typeNode = typeNode.firstChild
+        }
+        const type = typeNode.text
+        return new Parameter(mutableType, identifier, mapType(type), isNullable)
+    }
+}
 
-// Find primary constructor
-walkUntil('primary_constructor')
-const constructorNode = cursor.currentNode
-log(constructorNode.toString())
+const classNode = tree.rootNode.children.find(it => it.type === 'class_declaration')
+const className = classNode.children.find(it => it.type === 'type_identifier').text
+const constructorNode = classNode.children.find(it => it.type === 'primary_constructor')
+const parameters = constructorNode.children
+    .filter(it => it.type === 'class_parameter')
+    .map(it => createParameterFromNode(it))
 
-// For each constructor parameter
-cursor.gotoFirstChild()
-walkUntil('class_parameter')
-// TODO ...
+/**
+ * @param {string} className
+ * @param {Parameter[]} parameters
+ */
+function generateTypeScript(className, parameters) {
+    return `export class ${className} {\n`
+        + parameters
+            .map(it => {
+                const typeString = it.isNullable ? `${it.type} | null` : it.type
+                return `  ${it.identifier}: ${typeString}`
+            })
+            .join("\n")
+        + "\n}\n"
+}
+
+const tsOutput = generateTypeScript(className, parameters)
+log(tsOutput)
+fs.writeFileSync("out/apple.ts", tsOutput)
